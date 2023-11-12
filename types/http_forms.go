422a7ae1ff0df1154dc16e83baaea776faac9bef
SharkBeastGaming <81560132+SharkBeastGaming@users.noreply.github.com>
2023-11-12 20:05:45 +0530

    Refactor form bindings in SignUp struct

--- types/http_forms.go
+++ types/http_forms.go
@@ -2,12 +2,12 @@ package types
 
 // The SignUp struct is used to bind the request body form to the struct.
 type SignUp struct {
-	Username  string `json:"username" form:"username" binding:"required,min=4,max=32"`
-	Email     string `json:"email" form:"email" binding:"required,email"`
-	Password  string `json:"password" form:"password" binding:"required,min=8,max=32"`
-	FirstName string `json:"first_name" form:"first_name" binding:"required,min=4,max=32"`
-	LastName  string `json:"last_name" form:"last_name" binding:"required,min=4,max=32"`
-	Age       int    `json:"age" form:"age" binding:"required,min=1"`
+	Username  string `json:"username" binding:"required,min=4,max=32"`
+	Email     string `json:"email"  binding:"required,email"`
+	Password  string `json:"password"  binding:"required,min=8,max=32"`
+	FirstName string `json:"first_name"  binding:"required,min=4,max=32"`
+	LastName  string `json:"last_name"  binding:"required,min=4,max=32"`
+	Age       int    `json:"age"  binding:"required,min=1"`
 }
 
 // The Login struct is used to bind the request body form to the struct.
